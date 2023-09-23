47bccecbe1fe81acb439ef03c0e25388cb0add4f
Simon Peter <petersimon0730@gmail.com>
2023-09-23 04:55:40 +0100

    project  second commit

--- src/utils/post.js
+++ src/utils/post.js
@@ -2,7 +2,7 @@ const baseUrl = "https://backend.getlinked.ai";
 
 export const postForm = async (postData) => {
   try {
-    const response = await fetch("https://backend.getlinked.ai/hackathon/contact-form", {
+    const response = await fetch(`${baseUrl}/hackathon/contact-form`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
