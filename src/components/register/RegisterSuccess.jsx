146154f381bd80ca313c3ded1161c640b7d13ced
Simon Peter <petersimon0730@gmail.com>
2023-09-23 10:40:48 +0100

    more additions

--- src/components/register/RegisterSuccess.jsx
+++ src/components/register/RegisterSuccess.jsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 const RegisterSuccess = ({ onClose }) => {
   return (
-    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="flex items-center justify-center bg-black bg-opacity-50 inset-0 w-screen  h-screen p-5 z-50 fixed top-0 left-0">
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="flex items-center justify-center bg-scheme-dark bg-opacity-70 inset-0 w-screen  h-screen p-5 z-50 fixed top-0 left-0">
       <div className="border-scheme-violet border flex rounded-md p-5 lg:p-10 items-center justify-center gap-5 flex-col relative">
         <div className="flex gap-0 self-center relative">
           <Image
