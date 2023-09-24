39371ae2746bafd8b8b79b566b126e19a07ee379
Simon Peter <petersimon0730@gmail.com>
2023-09-24 17:50:45 +0100

    more additions

--- src/components/home/intro/CountdownTimer.jsx
+++ src/components/home/intro/CountdownTimer.jsx
@@ -10,7 +10,7 @@ const CountdownTimer = () => {
         hours: Math.floor(( difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor(( difference / 1000 / 60 ) % 60),
         seconds: Math.floor(( difference / 1000) % 60),
-      } e
+      }
     }
   }
   return (
